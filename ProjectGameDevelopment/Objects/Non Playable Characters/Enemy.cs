09e2bd2b6a29680ccb80a28b0a378118c3799929
Nestor Lenaerts <s122452@ap.be>
2022-12-26 02:11:20 +0100

    Enemy Toevoegen || NPC

---
+++ ProjectGameDevelopment/Objects/Non Playable Characters/Enemy.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectGameDevelopment.AnimationSection;
+using ProjectGameDevelopment.Characters.Playable;
+using ProjectGameDevelopment.MovementBehaviour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGameDevelopment.Characters
+{
+    public class Enemy : NPC, IJump
+    {
+        #region public fields
+        public Animation[] NPCAnimation;
+
+        public CurrentMovementState CurrentMovementState;
+        public AnimationMovement AnimationMovement;
+
+        #endregion
+
+        public Enemy(Texture2D _enemySpriteSheet, Rectangle _pathway, float speed, bool _canJump, bool _isInteligent, Player _player, Vector2 inteligentPosition)
+        {
+            this.IsInteligent = _isInteligent;
+
+            if (this.IsInteligent)
+            {
+                this.Position = new Vector2(inteligentPosition.X, inteligentPosition.Y);
+                Hitbox = new Rectangle((int)Position.X, (int)Position.Y, 25, 32);
+
+            }
+            else
+            {
+                this.Position = new Vector2(_pathway.X, _pathway.Y);
+                Hitbox = new Rectangle((int)this.Position.X, (int)this.Position.Y, 32, 32);
+
+            }
+
+
+            this.StartY = this.Velocity.Y;
+            this.Spritesheet = _enemySpriteSheet;
+            this.Pathway = _pathway;
+            this.Speed = speed;
+            this.isFacingRight = true;
+            this.Player = _player;
+            this.SpriteMoveDirection = SpriteEffects.None;
+            this.PathWayMovement = new PathWayMovement();
+            this.FollowingMovement = new Following_Movement();
+
+            //IN DEZE GAME KUNNEN DE VIJANDEN GEWOON NIET SHIETEN, dus ik vraag ze ook niet in de constructor parameters
+           
+            CanShoot = false;
+            IsShooting = false;
+
+
+            NPCAnimation = new Animation[1];                             //voor het moment maar 1 animatie
+            NPCAnimation[0] = new Animation(_enemySpriteSheet);
+
+            currentMovementState =  CurrentMovementState.Running;
+            AnimationMovement = new AnimationMovement(this.Spritesheet);
+
+
+
+            
+
+        }
+
+       
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            AnimationMovement.Draw(spriteBatch, this.Position, gameTime, this.SpriteMoveDirection);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //controleren welke type het is, INTELIGENT of PATHWAY TYPE
+            if(!this.IsInteligent)
+                PathWayMovement.EnemysMovement(this, this.Player);
+            else
+                FollowingMovement.EnemysMovement(this, this.Player);
+                           
+        }
+    }
+}
