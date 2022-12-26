393a6519cf218cacf46cea5eda31736d53d32907
Nestor Lenaerts <s122452@ap.be>
2022-12-26 02:09:29 +0100

    groene comment aangepast

--- ProjectGameDevelopment/InputControl/IReadInput.cs
+++ ProjectGameDevelopment/InputControl/IReadInput.cs
@@ -10,7 +10,7 @@ namespace ProjectGameDevelopment.InputControl
 {
     public interface IReadInput
     {
-        //dit is voor de charachter dat we willen de input van de keyboard lezen || rechten hebben, dus geen NPC
+        //dit is voor de charachter dat we willen de input van de keyboard lezen, dus geen NPC
         Vector2 ReadInput(Player player, GameTime gameTime);
     }
 }
