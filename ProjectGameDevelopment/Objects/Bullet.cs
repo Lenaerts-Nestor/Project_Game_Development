3dd2166d15c6755636d81ba680086d790b952826
Nestor Lenaerts <s122452@ap.be>
2022-12-26 02:11:53 +0100

    Bullet classe creeren

---
+++ ProjectGameDevelopment/Objects/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectGameDevelopment.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGameDevelopment.Objects
+{
+    public class Bullet: Characters.Object, IGameObject
+    {
+        public Bullet(Texture2D _bulletTexture, Vector2 _position, float _speed )
+        {
+            this.Spritesheet = _bulletTexture;
+            this.Speed = _speed;
+            this.Hitbox = new Rectangle((int)_position.X, (int)_position.Y, _bulletTexture.Width, _bulletTexture.Height);
+        }
+
+        //controleren als de bullet iets heeft geraakt
+        public bool BulletHit(Rectangle rect)
+        {
+            return Hitbox.Intersects(rect);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(this.Spritesheet, this.Hitbox, Color.White);
+        }
+        
+        public void Update(GameTime gameTime)
+        {
+            Hitbox.X += (int)Speed;
+        }
+    }
+}
