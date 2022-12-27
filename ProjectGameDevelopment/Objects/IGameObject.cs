dd207c9d0bd4d9872975b8a1f43e16685213d474
Nestor Lenaerts <s122452@ap.be>
2022-12-27 01:06:16 +0100

    Spaties aangepast

--- ProjectGameDevelopment/Objects/IGameObject.cs
+++ ProjectGameDevelopment/Objects/IGameObject.cs
@@ -13,6 +13,13 @@ namespace ProjectGameDevelopment.Characters
     public interface IGameObject
     {
         
+        /// <summary>
+        /// een Object waar je iets mee kan doen of clicken, 
+        /// het concept object in deze game kan zijn een Entiteit, een MenuButton , een rectangle van een map, etc...
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="gameTime"></param>
+
         void Draw(SpriteBatch spriteBatch, GameTime gameTime);
         void Update(GameTime gameTime);
 
