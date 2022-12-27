fbb63d8f40933ac7f2431a483ed8095111a05d45
Nestor Lenaerts <s122452@ap.be>
2022-12-27 03:00:37 +0100

    poging buff item creeren

---
+++ ProjectGameDevelopment/Objects/BuffItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ProjectGameDevelopment.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Object = ProjectGameDevelopment.Characters.Object;
+
+namespace ProjectGameDevelopment.Objects
+{
+    public class BuffItem : Object, IGameObject
+    {
+        public BuffItem(Texture2D _bulletTexture, Vector2 _position, float _speed)
+        {
+            this.Spritesheet = _bulletTexture;
+            this.Speed = _speed;
+            this.Hitbox = new Rectangle((int)_position.X, (int)_position.Y, _bulletTexture.Width, _bulletTexture.Height);
+        }
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            spriteBatch.Draw(this.Spritesheet, this.Hitbox, Color.White);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Hitbox.X += (int)Speed;
+        }
+    }
+}
